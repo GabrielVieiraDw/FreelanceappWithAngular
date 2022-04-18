ff712cee824992d5d44717b278a759246c5626d1
Gabriel Vieira <gabriel.vieira@eitv.com.br>
2022-04-18 20:22:19 -0300

    Added proposals feature

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   selector: 'app-documents',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
-
+  pageTitle: string = "Documents Dashboard"
+  
+  documents: Document[] = [
+    {
+      title: "My first Doc",
+      description: 'First description',
+      file_url: 'https://google.com',
+      updated_at: '18/04/2022',
+      image_url: 'https://google.com',
+    },
+    {
+      title: "My second Doc",
+      description: 'First description',
+      file_url: 'https://google.com',
+      updated_at: '18/04/2022',
+      image_url: 'https://google.com',
+    },
+    {
+      title: "My last Doc",
+      description: 'First description',
+      file_url: 'https://google.com',
+      updated_at: '18/04/2022',
+      image_url: 'https://google.com',
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/proposal/proposal-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proposal',
+  templateUrl: './proposal-list.component.html',
+  styleUrls: ['./proposal-list.component.css']
+})
+export class ProposalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
