01ad510bf36ff995689b3577d74157b75a0329cb
Gabriel Vieira <gabriel.vieira@eitv.com.br>
2022-04-22 14:20:16 -0300

    Start styling the app

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent implements OnInit {
       description: 'First description',
       file_url: 'https://google.com',
       updated_at: '18/04/2022',
-      image_url: 'https://google.com',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
     },
     {
       title: "My second Doc",
-      description: 'First description',
+      description: 'Second description',
       file_url: 'https://google.com',
       updated_at: '18/04/2022',
-      image_url: 'https://google.com',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
     },
     {
       title: "My last Doc",
-      description: 'First description',
+      description: 'Second description',
       file_url: 'https://google.com',
       updated_at: '18/04/2022',
-      image_url: 'https://google.com',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
     },
   ]
   constructor() { }
